36c904db1e2d7e4d2d2c753c62fdaa221ce5f831
Sania Hamid <hamid.sania@gmail.com>
2020-12-07 13:35:13 -0500

    first commit

---
+++ Chapter2/assertion.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 13:11:32 2020
+
+@author: shamid
+
+This code shows an example of "assert" statement.
+It is a debuggin aid that tests a condition. 
+If the assert condition is true then nothing happens and the program continues 
+to execute as normal. But if the condition evaluated to false, an
+AssertionError is raised with an optional error message.
+"""
+
