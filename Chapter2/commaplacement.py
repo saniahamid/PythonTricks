d86b28ba3b0f76e1ae1e7f5c4040c032f0fcb417
Sania Hamid <hamid.sania@gmail.com>
2020-12-09 13:06:30 -0500

    comma placement

---
+++ Chapter2/commaplacement.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 13:02:10 2020
+
+@author: shamid
+
+you can  place a comma after every item in a list, dict or set constant,
+including the last item.
+"""
+
+names = [ "Alice",
+         "Bob",
+         "Camme",
+         "Dilbert",
+        ]
+print(names)
+
